a973db62efd88448d4857992d05b7dddd29fb656
TamandeepSingh <tamandeepsinghkhamba@gmail.com>
2017-11-06 10:32:42 +0530

    commits server file

---
+++ server.js
@@ -0,0 +1,28 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var morgan = require("morgan");
+var config = require("./config/config.js");
+
+var app = express()
+
+// listening port
+app.listen(3000, function(err){
+  if(err){
+    console.log(err)
+  }
+  else{
+    console.log("Listening to port 3000")
+  }
+});
+
+// dependencies
+app.use(bodyParser.urlencoded({ extented : true }));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+// getting in views in public html file
+app.use(express.static(__dirname + '/public/views'));
+
+app.get('', function(req,res){
+	res.sendFile(__dirname + '/public/views/index.html');
+});
