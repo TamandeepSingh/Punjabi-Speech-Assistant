281f943c22c6b42b9c7f195d199e9768a3dd487b
shamysekhon <shamy.sekhon@me.com>
2017-11-08 09:31:33 +0530

    css

--- public/views/js/reco.js
+++ public/views/js/reco.js
@@ -1,24 +1,24 @@
 var sira = new Artyom();
 
-var langs = ["hi-IN","en-GB"];
-function startReco(){
+var langs = ["hi-IN", "en-GB"];
+
+function startReco() {
     var picGif = document.getElementById("gif");
     picGif.innerHTML = "<img src=\"../gifs/speaking.gif\" width=\"400px\" height=\"75px\">";
     sira.initialize({
-      lang: langs[0],
-      //lang: langs[1],
-      //lang:langs,  // hi-IN  //en-GB
-      debug:true, // Show what recognizes in the Console
-      listen:true, // Start listening after this
-      speed:0.9, // Talk a little bit slow
-      mode:"normal" // This parameter is not required as it will be normal by default
-});
+        lang: langs[0],
+        //lang: langs[1],
+        //lang:langs,  // hi-IN  //en-GB
+        debug: true, // Show what recognizes in the Console
+        listen: true, // Start listening after this
+        speed: 0.9, // Talk a little bit slow
+        mode: "normal" // This parameter is not required as it will be normal by default
+    });
 }
 
-sira.addCommands([
-    {
+sira.addCommands([{
         indexes: ["खोलो Facebook"],
-        action: function(i){
+        action: function(i) {
             console.log("खोलो Facebook");
             var eg = "ਖਲੋ Facebook";
             var res = document.getElementById("result");
@@ -28,7 +28,7 @@ sira.addCommands([
     },
     {
         indexes: ["Facebook खोलो"],
-        action: function(i){
+        action: function(i) {
             console.log("Facebook खोल");
             var eg = "Facebook ਖਲੋ";
             var res = document.getElementById("result");
@@ -38,17 +38,29 @@ sira.addCommands([
     },
     {
         indexes: ["Facebook खोलो"],
-        action: function(i){
+        action: function(i) {
             console.log("Facebook खोल");
             var eg = "Facebook ਖਲੋ";
             var res = document.getElementById("result");
             res.innerHTML = eg;
-            window.open("https://www.facebook.com/",'_blank');
+            window.open("https://www.facebook.com/", '_blank');
+        }
+    },
+
+    {
+        indexes: ["Facebook खोल"],
+        action: function(i) {
+            console.log("Facebook खोल");
+            var eg = "Facebook ਖਲੋ";
+            var res = document.getElementById("result");
+            res.innerHTML = eg;
+            window.open("https://www.facebook.com/", '_blank');
         }
     }
+
 ]);
 
-function stopReco(){
+function stopReco() {
     var picGif = document.getElementById("gif");
     picGif.innerHTML = "";
     var res = document.getElementById("result");
